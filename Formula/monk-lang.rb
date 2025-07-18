f58cc565e7c1b72f3384f3cc8c0cb6319d151c86
Sameer Khan <sameer.khan@theinternetfolks.com>
2025-07-19 00:37:43 +0530

    Fix Homebrew formula: update checksums and binary structure
    - Updated SHA256 checksums to match corrected GitHub release assets
    - Fixed binary name in archives (monk instead of versioned names)
    - Tested successfully with brew install --build-from-source

--- Formula/monk-lang.rb
+++ Formula/monk-lang.rb
@@ -6,28 +6,28 @@ class MonkLang < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-darwin-x64.tar.gz"
-      sha256 "50b6ffa4166b87f0bb85057c2f0eba6aad213e4ad4ce9b417d906354d849d0d0"
+      sha256 "fffc9815fc266965f648b2d0744ce10d32b696b976d3f50b76e5ae505b9d73e2"
     else
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-darwin-arm64.tar.gz"
-      sha256 "8830dfff1da997281307044b1e9346c367ba885aab58849a6db8ca0cfaae4838"
+      sha256 "00ac81ea5d23bf8832db2ac4a0808aa66b4a7680e808942c31a67061580f6a4c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-linux-x64.tar.gz"
-      sha256 "ff42794a172446499784e090fb8081424fe0c759b1a619029aa2e7d664047f10"
+      sha256 "5760244613f71b10101b801c6353a77d0e1d045b8efd798faf7a3f6dc738ef65"
     else
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-linux-arm64.tar.gz"
-      sha256 "68583c58008cb592b8804a47f2e66f2d6f2effb4ad735fd73e9d0bce9fd2772b"
+      sha256 "6d22446e61d9127350911d386c39ede2f3021461b216b98f24440322ec7dd089"
     end
   end
 
   license "MIT"
 
   def install
-    # The binary name format in the tarball
-    bin.install "monk-#{os}-#{Hardware::CPU.arch}" => "monk"
+    # The binary is simply named "monk" in the tarball
+    bin.install "monk"
   end
 
   test do
