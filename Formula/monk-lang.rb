0586a092be64bd52458d7bde522ebb2ab275d196
Sameer Khan <sameer.khan@theinternetfolks.com>
2025-07-24 14:40:17 +0530

    Update monk-lang to v0.0.6

--- Formula/monk-lang.rb
+++ Formula/monk-lang.rb
@@ -1,25 +1,25 @@
 class MonkLang < Formula
   desc "A minimalist, readable, and performant programming language"
   homepage "https://github.com/monkfromearth/monk-lang"
-  version "0.0.5"
+  version "0.0.6"
   
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-darwin-x64.tar.gz"
-      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.5-darwin-x64.tar.gz in checksums"
+      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.6-darwin-x64.tar.gz in checksums file"
     else
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-darwin-arm64.tar.gz"
-      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.5-darwin-arm64.tar.gz in checksums"
+      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.6-darwin-arm64.tar.gz in checksums file"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-linux-x64.tar.gz"
-      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.5-linux-x64.tar.gz in checksums"
+      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.6-linux-x64.tar.gz in checksums file"
     else
       url "https://github.com/monkfromearth/monk-lang/releases/download/v#{version}/monk-v#{version}-linux-arm64.tar.gz"
-      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.5-linux-arm64.tar.gz in checksums"
+      sha256 "[0;31m[ERROR][0m Could not find SHA256 for monk-v0.0.6-linux-arm64.tar.gz in checksums file"
     end
   end
 
